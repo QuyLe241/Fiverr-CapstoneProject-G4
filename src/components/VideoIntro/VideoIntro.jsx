dc33adfe5fd4cd1c7df2d1b818355167b1803dff
hao <macanhhao0347683890@gmail.com>
2024-09-13 19:05:12 +0700

    13 09

--- src/components/VideoIntro/VideoIntro.jsx
+++ src/components/VideoIntro/VideoIntro.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import BannerIntro from "./BannerIntro";
+import LogoMakec from "./LogoMake";
+import LogoMake from "./LogoMake";
 
 const VideoIntro = () => {
-  return <div>Bao gồm: các video, What they're saying about Fiverr</div>;
+  return (
+    <div className="w-full py-6">
+      <video
+  crossOrigin="anonymous"
+  src="/img/How Fiverr Works EN Subs 16x9.mp4"
+  poster="/img/thumbnail.jpg"
+  role="video"
+  preload="auto"
+  controls
+  autoPlay 
+  muted 
+  width={2000}
+  className="rounded-lg"
+>
+  <source role="source" src="/img/How Fiverr Works EN Subs 16x9.mp4" type="video/mp4" />
+</video>
+
+    <BannerIntro/>
+    <LogoMake/>
+    </div>
+  );
 };
 
 export default VideoIntro;
