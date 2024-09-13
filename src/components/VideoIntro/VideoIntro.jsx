84e7df2212cab0136d7b54a0d44b2f9982f6839e
QUY666666 <lenhatquy2401@gmail.com>
2024-09-13 23:20:03 +0700

    update config

--- src/components/VideoIntro/VideoIntro.jsx
+++ src/components/VideoIntro/VideoIntro.jsx
@@ -5,24 +5,28 @@ import LogoMake from "./LogoMake";
 
 const VideoIntro = () => {
   return (
-    <div className="w-full py-6">
+    <div className="w-full py-6 container mx-auto px-2">
       <video
-  crossOrigin="anonymous"
-  src="/img/How Fiverr Works EN Subs 16x9.mp4"
-  poster="/img/thumbnail.jpg"
-  role="video"
-  preload="auto"
-  controls
-  autoPlay 
-  muted 
-  width={2000}
-  className="rounded-lg"
->
-  <source role="source" src="/img/How Fiverr Works EN Subs 16x9.mp4" type="video/mp4" />
-</video>
+        crossOrigin="anonymous"
+        src="/img/How Fiverr Works EN Subs 16x9.mp4"
+        poster="/img/thumbnail.jpg"
+        role="video"
+        preload="auto"
+        controls
+        autoPlay
+        muted
+        width={2000}
+        className="rounded-lg"
+      >
+        <source
+          role="source"
+          src="/img/How Fiverr Works EN Subs 16x9.mp4"
+          type="video/mp4"
+        />
+      </video>
 
-    <BannerIntro/>
-    <LogoMake/>
+      <BannerIntro />
+      <LogoMake />
     </div>
   );
 };
