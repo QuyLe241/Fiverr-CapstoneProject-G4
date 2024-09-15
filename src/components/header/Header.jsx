c89ea475634a7c2fa44045b78e492dc058d2d342
QUY666666 <lenhatquy2401@gmail.com>
2024-09-15 14:58:09 +0700

    merge

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -17,46 +17,19 @@ const items = [
   {
     key: "1",
     label: (
-      <a
-        target="_blank"
-        rel="noopener noreferrer"
-        href="https://www.antgroup.com"
-      >
+      <a target="_blank" rel="noopener noreferrer" href="#">
         1st menu item
       </a>
     ),
   },
   {
     key: "2",
+    // danger: true,
     label: (
-      <a
-        target="_blank"
-        rel="noopener noreferrer"
-        href="https://www.aliyun.com"
-      >
-        2nd menu item (disabled)
+      <a target="_blank" href="/dang-nhap">
+        2 item
       </a>
     ),
-    icon: <SmileOutlined />,
-    disabled: true,
-  },
-  {
-    key: "3",
-    label: (
-      <a
-        target="_blank"
-        rel="noopener noreferrer"
-        href="https://www.luohanacademy.com"
-      >
-        3rd menu item (disabled)
-      </a>
-    ),
-    disabled: true,
-  },
-  {
-    key: "4",
-    danger: true,
-    label: "a danger item",
   },
 ];
 
