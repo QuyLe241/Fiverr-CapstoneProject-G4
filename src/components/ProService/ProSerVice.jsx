3119c0a15728d1651ce9a3d784c999d9bd2be2ce
QUY666666 <lenhatquy2401@gmail.com>
2024-09-21 17:26:42 +0700

    update proservices 12/9 5:26

--- src/components/ProService/ProSerVice.jsx
+++ src/components/ProService/ProSerVice.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./style.scss";
+import Slider from "react-slick";
 import BannerConnect from "../../assets/Img/bannerConnect1.png";
 import connectImg from "../../assets/Img/connectImg.png";
 import processImg1 from "../../assets/Img/processImg1.png";
@@ -13,8 +14,18 @@ import benefitsImg4 from "../../assets/Img/benefitsImg4.png";
 import benefitsImg5 from "../../assets/Img/benefitsImg5.png";
 import benefitsImg6 from "../../assets/Img/benefitsImg6.png";
 import checkImg from "../../assets/Img/checkImg.png";
+import proStories1 from "../../assets/Img/prostoriesImg1.png";
+import proStories2 from "../../assets/Img/prostoriesImg2.png";
+import proStories3 from "../../assets/Img/prostoriesImg3.png";
 
 const ProSerVice = () => {
+  var settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
   return (
     <div>
       {/* Banner */}
@@ -25,7 +36,7 @@ const ProSerVice = () => {
           height: "450px",
         }}
       >
-        <div className="container mx-auto px-2 flex items-center justify-around">
+        <div className="container mx-auto px-2 flex items-center justify-around py-5">
           <div className="text-white space-y-5">
             <h1>
               <div className="">
@@ -56,8 +67,8 @@ const ProSerVice = () => {
 
       {/* Benefits */}
       <div className="benefits container max-w-[1110px] mx-auto px-2 ">
-        <div className="">
-          <div className="">
+        <div className="py-16 px-6">
+          <div className="pt-3 pb-9">
             <h3
               className="text-center"
               style={{ fontSize: "28px", fontWeight: 600 }}
@@ -65,7 +76,7 @@ const ProSerVice = () => {
               Bring on the benefits
             </h3>
           </div>
-          <div className="text-center flex justify-center space-x-3">
+          <div className="text-center flex justify-center space-x-3 pt-3 pb-9">
             <div className="w-1/3">
               <div className="flex justify-center">
                 <img
@@ -75,14 +86,14 @@ const ProSerVice = () => {
                   alt=""
                 />
               </div>
-              <div className="">
+              <div className="mt-5">
                 <span
                   className=""
                   style={{ fontWeight: 600, fontSize: "18px" }}
                 >
                   Pro badge
                 </span>
-                <p style={{ color: "#62646a" }}>
+                <p className="mt-3" style={{ color: "#62646a" }}>
                   Stand out with a blue Pro badge on your profile and services,
                   signifying your vetted status on Fiverr.
                 </p>
@@ -92,14 +103,14 @@ const ProSerVice = () => {
               <div className="flex justify-center">
                 <img src={benefitsImg2} alt="" />
               </div>
-              <div className="">
+              <div className="mt-6">
                 <span
                   className=""
                   style={{ fontWeight: 600, fontSize: "18px" }}
                 >
                   Access to premium clients
                 </span>
-                <p style={{ color: "#62646a" }}>
+                <p className="mt-3" style={{ color: "#62646a" }}>
                   Take your place in a curated Pro catalog. Connect with clients
                   looking for bigger, ongoing projects.
                 </p>
@@ -109,33 +120,33 @@ const ProSerVice = () => {
               <div className="flex justify-center">
                 <img src={benefitsImg3} alt="" />
               </div>
-              <div className="">
+              <div className="mt-5">
                 <span
                   className=""
                   style={{ fontWeight: 600, fontSize: "18px" }}
                 >
                   Get paid by the hour
                 </span>
-                <p style={{ color: "#62646a" }}>
+                <p className="mt-3" style={{ color: "#62646a" }}>
                   Expand your offerings to support flexible project scopes and
                   build ongoing, long-term relationships.
                 </p>
               </div>
             </div>
           </div>
-          <div className="flex text-center justify-center space-x-3">
+          <div className="flex text-center justify-center space-x-3 pt-5 pb-9">
             <div className="w-1/3">
               <div className="flex justify-center">
                 <img src={benefitsImg4} alt="" />
               </div>
-              <div className="">
+              <div className="mt-5">
                 <span
                   className=""
                   style={{ fontWeight: 600, fontSize: "18px" }}
                 >
                   Onboarding support
                 </span>
-                <p style={{ color: "#62646a" }}>
+                <p className="mt-3" style={{ color: "#62646a" }}>
                   Kickstart your Pro journey with a dedicated session where
                   we’ll go over expectations, benefits, and tips to work
                   effectively with business clients.
@@ -146,14 +157,14 @@ const ProSerVice = () => {
               <div className="flex justify-center">
                 <img src={benefitsImg5} alt="" />
               </div>
-              <div className="">
+              <div className="mt-5">
                 <span
                   className=""
                   style={{ fontWeight: 600, fontSize: "18px" }}
                 >
                   Exclusive community
                 </span>
-                <p style={{ color: "#62646a" }}>
+                <p className="mt-3" style={{ color: "#62646a" }}>
                   Join our Pro-only hub for strategic partnerships, upskilling
                   opportunities, and perks.
                 </p>
@@ -163,14 +174,14 @@ const ProSerVice = () => {
               <div className="flex justify-center">
                 <img src={benefitsImg6} alt="" />
               </div>
-              <div className="">
+              <div className="mt-5">
                 <span
                   className=""
                   style={{ fontWeight: 600, fontSize: "18px" }}
                 >
                   Customer protection
                 </span>
-                <p style={{ color: "#62646a" }}>
+                <p className="mt-3" style={{ color: "#62646a" }}>
                   We have your back. If a Pro client is unsatisfied and cancels
                   an order, you’ll still get paid within 14 days*.
                 </p>
@@ -192,16 +203,16 @@ const ProSerVice = () => {
           <div className="">
             <h3
               className="text-center"
-              style={{ fontSize: "25px", fontWeight: 600 }}
+              style={{ fontSize: "25px", fontWeight: 700 }}
             >
               The application process
             </h3>
-            <p className="text-center" style={{ color: "#74767E" }}>
+            <p className="text-center mt-5" style={{ color: "#74767E" }}>
               To pass the Pro test, freelancers undergo a multi-stage process
               which includes hand-vetting by our Talent Evaluators.
             </p>
           </div>
-          <div className="flex justify-around">
+          <div className="flex justify-around mt-6">
             <div className="w-1/4">
               <img src={processImg1} alt="" />
             </div>
@@ -219,29 +230,223 @@ const ProSerVice = () => {
       </div>
 
       {/* how we vet */}
-      <div className="container mx-auto px-2 max-w-[1110px]">
+      <div className="container py-16 mx-auto px-2 max-w-[1110px]">
         <div className="">
-          <h3>Here’s how we vet freelancers</h3>
+          <h3
+            className="text-center pb-10"
+            style={{ fontWeight: 600, fontSize: "28px" }}
+          >
+            Here’s how we vet freelancers
+          </h3>
         </div>
-        <div className="">
-          <div className="">
-            <img src="" alt="" />
+        <div className="flex space-x-3">
+          <div className="w-1/3">
+            <div className="flex justify-center">
+              <img src={checkImg} alt="" />
+            </div>
+            <div className="text-center">
+              <span className="" style={{ fontSize: "16px", fontWeight: 600 }}>
+                Skills & expertise
+              </span>
+              <p style={{ fontSize: "15px", color: "#62646a" }}>
+                Significant experience working in relevant Pro fields
+              </p>
+            </div>
           </div>
-          <div className="">
-            <span></span>
-            <p></p>
+          <div className="w-1/3">
+            <div className="flex justify-center">
+              <img src={checkImg} alt="" />
+            </div>
+            <div className="text-center">
+              <span className="" style={{ fontSize: "16px", fontWeight: 600 }}>
+                Skills & expertise
+              </span>
+              <p style={{ fontSize: "15px", color: "#62646a" }}>
+                Significant experience working in relevant Pro fields
+              </p>
+            </div>
+          </div>
+          <div className="w-1/3">
+            <div className="flex justify-center">
+              <img src={checkImg} alt="" />
+            </div>
+            <div className="text-center">
+              <span className="" style={{ fontSize: "16px", fontWeight: 600 }}>
+                Skills & expertise
+              </span>
+              <p style={{ fontSize: "15px", color: "#62646a" }}>
+                Significant experience working in relevant Pro fields
+              </p>
+            </div>
           </div>
         </div>
-        <div className=""></div>
-        <div className=""></div>
+        <div className="flex space-x-3 pt-16">
+          <div className="w-1/3">
+            <div className="flex justify-center">
+              <img src={checkImg} alt="" />
+            </div>
+            <div className="text-center">
+              <span className="" style={{ fontSize: "16px", fontWeight: 600 }}>
+                Skills & expertise
+              </span>
+              <p style={{ fontSize: "15px", color: "#62646a" }}>
+                Significant experience working in relevant Pro fields
+              </p>
+            </div>
+          </div>
+          <div className="w-1/3">
+            <div className="flex justify-center">
+              <img src={checkImg} alt="" />
+            </div>
+            <div className="text-center">
+              <span className="" style={{ fontSize: "16px", fontWeight: 600 }}>
+                Skills & expertise
+              </span>
+              <p style={{ fontSize: "15px", color: "#62646a" }}>
+                Significant experience working in relevant Pro fields
+              </p>
+            </div>
+          </div>
+          <div className="w-1/3">
+            <div className="flex justify-center">
+              <img src={checkImg} alt="" />
+            </div>
+            <div className="text-center">
+              <span className="" style={{ fontSize: "16px", fontWeight: 600 }}>
+                Skills & expertise
+              </span>
+              <p style={{ fontSize: "15px", color: "#62646a" }}>
+                Significant experience working in relevant Pro fields
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="flex space-x-2 py-16">
+          <div className="w-1/2">
+            <div className="flex justify-center">
+              <img src={checkImg} alt="" />
+            </div>
+            <div className="text-center">
+              <span className="" style={{ fontSize: "16px", fontWeight: 600 }}>
+                Skills & expertise
+              </span>
+              <p style={{ fontSize: "15px", color: "#62646a" }}>
+                Significant experience working in relevant Pro fields
+              </p>
+            </div>
+          </div>
+          <div className="w-1/2">
+            <div className="flex justify-center">
+              <img src={checkImg} alt="" />
+            </div>
+            <div className="text-center">
+              <span className="" style={{ fontSize: "16px", fontWeight: 600 }}>
+                Skills & expertise
+              </span>
+              <p style={{ fontSize: "15px", color: "#62646a" }}>
+                Significant experience working in relevant Pro fields
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Pro stories */}
+      <div className="" style={{ backgroundColor: "#fafafa" }}>
+        <div className="container mx-auto px-2 pt-9 pb-16">
+          <div className="text-center py-8">
+            <h3 style={{ fontSize: "25px", fontWeight: 600 }}>Pro stories</h3>
+            <p
+              className="mt-3"
+              style={{ fontSize: "16px", fontWeight: "500", color: "#62646a" }}
+            >
+              {" "}
+              See how Pro freelancers are achieving success
+            </p>
+          </div>
+          {/* carousel */}
+          <div className="stories_carousel flex justify-center">
+            <div className="div_carousel w-2/3">
+              <Slider className="p-3" {...settings}>
+                <div className=" style_carousel" style={{}}>
+                  <div className="flex justify-around items-center">
+                    <div className="">
+                      <img src={proStories1} alt="" />
+                    </div>
+                    <div className="text-center">
+                      <p className="name">Alan Redondo</p>
+                      <p className="decs">From broke to breaking the bank</p>
+                      <button
+                        className="py-2 px-5 rounded-md mt-8 hover:bg-slate-100"
+                        style={{ border: "1px solid black", fontWeight: 600 }}
+                      >
+                        Read story
+                      </button>
+                    </div>
+                  </div>
+                </div>
+                <div className="style_carousel ">
+                  <div className="flex justify-around items-center">
+                    <div className="">
+                      <img src={proStories2} alt="" />
+                    </div>
+                    <div className="text-center">
+                      <p className="name">Jacqui-Lee Katz</p>
+                      <p className="decs">From broke to breaking the bank</p>
+                      <button
+                        className="py-2 px-5 rounded-md mt-8 hover:bg-slate-100"
+                        style={{ border: "1px solid black", fontWeight: 600 }}
+                      >
+                        Read story
+                      </button>
+                    </div>
+                  </div>
+                </div>
+                <div className="style_carousel ">
+                  <div className="flex justify-around items-center">
+                    <div className="">
+                      <img src={proStories3} alt="" />
+                    </div>
+                    <div className=" text-center">
+                      <p className="name">Heather Metcalfe</p>
+                      <p className="decs">
+                        Unleashing creativity and building long-term connections
+                      </p>
+                      <button
+                        className="py-2 px-5 rounded-md mt-8 hover:bg-slate-100"
+                        style={{ border: "1px solid black", fontWeight: 600 }}
+                      >
+                        Read story
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </Slider>
+            </div>
+          </div>
+        </div>
       </div>
 
       {/* Apply  */}
       <div className="" style={{ backgroundColor: "rgba(0, 57, 18, 1)" }}>
-        <div
-          className="container mx-auto px-2"
-          style={{ height: "340px" }}
-        ></div>
+        <div className="container mx-auto px-2" style={{ height: "340px" }}>
+          <div className="text-center">
+            <p
+              className="text-white py-3"
+              style={{ fontSize: "55px", fontWeight: 600 }}
+            >
+              Apply to become a Fiverr <br /> Pro freelancer
+            </p>
+          </div>
+          <div className="flex justify-center mt-10">
+            <button
+              className=" py-2 px-8 bg-white rounded-lg hover:opacity-90"
+              style={{ fontWeight: 600, bottom: "" }}
+            >
+              Apply
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
